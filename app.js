81577b7b3d0999a57f874631bcdf02af4952121b
Sergey Khomenko <khomenko.mail@gmail.com>
2018-06-30 11:37:21 +0300

    Basic express app

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(8080, () => {
+    console.log('App started on http://localhost:8080/');
+});
