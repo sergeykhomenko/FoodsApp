c068b3613dd005ba03f370f8a9c684d7ed0c3748
Sergey Khomenko <khomenko.mail@gmail.com>
2018-07-06 16:08:50 +0300

    Register, login... No-react

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import mongoose from 'mongoose';
+import './server/models/User';
+
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import passportLocalMongoose from 'passport-local-mongoose';
+
 import * as db from './server/utils/DatabaseUtils';
 
 // API connect
@@ -12,11 +19,63 @@ db.setupConnection();
 
 const app = express();
 app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({extended:true}) );
+app.set('view engine', 'ejs');
+
+// Auth
+app.use(passport.initialize());
+app.use(passport.session());
+
+const User = mongoose.model('User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+
+app.use(require('express-session')({
+    secret: "Foods App Secret",
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.send('Hello world');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile( __dirname + '/public/login.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile( __dirname + '/public/register.html');
+});
+
+app.post('/register', (req, res) => {
+    User.register(
+        new User({ username: req.body.username }),
+        req.body.password,
+        (err, user) => {
+            if(err) {
+                console.log(err);
+                return res.sendFile(__dirname + '/public/register.html');
+            }
+
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/dashboard");
+            });
+        }
+    );
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect('/login');
 });
 
 app.use('/api', MainAPIRouter );
--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import passportLocalMongoose from 'passport-local-mongoose';
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    email : { type: String, required: true },
+    username: { type: String, required: true },
     name: { type: String },
     balance: { type: Number }
 });
+
+UserSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User', UserSchema);
